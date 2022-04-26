a0da1b70832abc576aeaf5ae437175a87c979324
Viktoriia Romanova <viktoriia_romanova@epam.com>
2022-04-26 15:08:34 +0300

    dropdown, popup model and header

--- static/js/store.js
+++ static/js/store.js
@@ -1,9 +1,28 @@
 $(document).ready(function () {
-    $('.item').hover(function () {
+    $('#shopping_cart').click(function () {
+        $('#order').show()
+    })
+
+    $('#close').click(function () {
+        $('#order').hide()
+    })
+
+    $('.category-bar_btn').click(function () {
+        $('.category-bar_btn').removeClass('active')
+        $(this).addClass('active')
+    })
+
+    var item = $('.item');
+
+    item.hover(function () {
             $(this).children().hide();
             $(this).css('background-image', $(this).children('.item-img').css('background-image'));
         }, function () {
             $(this).children().show();
             $(this).css('background-image', 'none')
-    });
+    })
+
+    item.click(function () {
+
+    })
 })
