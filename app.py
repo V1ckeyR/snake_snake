4a37c78106ad0959fba192a02fc3b8f555653e32
Viktoriia Romanova <viktoriia_romanova@epam.com>
2022-04-28 15:05:41 +0300

    shopping cart frontend

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from core.game import Field
 
 
 app = Flask(__name__, template_folder="templates", static_folder="static")
-# eventlet.monkey_patch(thread=False)
-# socket_io = SocketIO(app, async_mode='eventlet')
+eventlet.monkey_patch(thread=False)
+socket_io = SocketIO(app, async_mode='eventlet')
 
 game = Field()
 available = ['r', 'o', 'y', 'g', 'c', 'b', 'v']
@@ -24,58 +24,56 @@ def skins_store():
     return render_template('store.html', items=range(20))
 
 
-app.run()
+@socket_io.on('message')
+def handle_message(data):
+    print('Message: ' + data)
 
-# @socket_io.on('message')
-# def handle_message(data):
-#     print('Message: ' + data)
-#
-#
-# @socket_io.on('start game')
-# def handle_start_game():
-#     try:
-#         player = available.pop(0)
-#         clients[request.sid] = player
-#         game.add_player(player)
-#         emit('field', game.field, broadcast=True)
-#         emit('personal data', player)
-#     except IndexError:
-#         emit('limit')  # TODO
-#
-#
-# @socket_io.on('game')
-# def handle_game(key):
-#     player = clients[request.sid]
-#     if player not in movements.keys():
-#         movements[player] = key
-#
-#     results = game.move_snakes(movements)
-#     emit('personal score', results[player][1])
-#     if results['game over']:
-#         emit('win', broadcast=True)  # TODO
-#         game.clear()
-#
-#     if not results[player][0]:
-#         clients.pop(request.sid)
-#         available.append(player)
-#         emit('dead')
-#
-#     movements.clear()
-#     game.show()
-#     emit('field', game.field, broadcast=True)
-#
-#
-# @socket_io.on('connect')
-# def connect():
-#     print('Client connected')
-#     emit('field', game.field)
-#
-#
-# @socket_io.on('disconnect')
-# def disconnect():
-#     print('Client disconnected')
-#     snake = clients[request.sid]
-#     game.remove_player(snake)
-#     available.append(snake)
-#     clients.pop(request.sid)
-#     emit('field', game.field, broadcast=True)
+
+@socket_io.on('start game')
+def handle_start_game():
+    try:
+        player = available.pop(0)
+        clients[request.sid] = player
+        game.add_player(player)
+        emit('field', game.field, broadcast=True)
+        emit('personal data', player)
+    except IndexError:
+        emit('limit')  # TODO
+
+
+@socket_io.on('game')
+def handle_game(key):
+    player = clients[request.sid]
+    if player not in movements.keys():
+        movements[player] = key
+
+    results = game.move_snakes(movements)
+    emit('personal score', results[player][1])
+    if results['game over']:
+        emit('win', broadcast=True)  # TODO
+        game.clear()
+
+    if not results[player][0]:
+        clients.pop(request.sid)
+        available.append(player)
+        emit('dead')
+
+    movements.clear()
+    game.show()
+    emit('field', game.field, broadcast=True)
+
+
+@socket_io.on('connect')
+def connect():
+    print('Client connected')
+    emit('field', game.field)
+
+
+@socket_io.on('disconnect')
+def disconnect():
+    print('Client disconnected')
+    snake = clients[request.sid]
+    game.remove_player(snake)
+    available.append(snake)
+    clients.pop(request.sid)
+    emit('field', game.field, broadcast=True)
--- static/js/store.js
+++ static/js/store.js
@@ -1,28 +1,67 @@
 $(document).ready(function () {
+    var order = Array();
+    var item = $('.item');
+    var orderContent = $('#order-content');
+    var orderButtons = $('#order-buttons');
+
+    function drawOrderList() {
+        orderButtons.html('')
+        if (!order.length) {
+            orderContent.html('<span>Oops, it\'s nothing here :(</span>')
+        } else {
+            orderContent.html('')
+            for (let i in order) {
+                var id = order[i]
+                orderContent.append(`<div class="order-card">
+                    <span>${ parseInt(i) + 1 }</span>
+                    <div class="item-img"></div>
+                    <div class="dotted-line"></div>
+                    <span>$00.50</span>
+                    <span id="close_${id}" class="close">&times;</span>
+                </div>`)
+                $(`#close_${id}`).click( function () {
+                    var id =  $(this).attr('id').split('_')[1]
+                    order.splice(order.indexOf(id), 1);
+                    $(`#${id}`).children('.tip_item').html('+');
+                    drawOrderList();
+                })
+            }
+            orderButtons.append('<button id="submit_order" type="submit">Submit</button>')
+        }
+    }
+
     $('#shopping_cart').click(function () {
         $('#order').show()
+        drawOrderList()
     })
 
-    $('#close').click(function () {
-        $('#order').hide()
+    $('#close_shopping_cart').click(function () {
+        $('#order').hide();
     })
 
     $('.category-bar_btn').click(function () {
-        $('.category-bar_btn').removeClass('active')
-        $(this).addClass('active')
+        $('.category-bar_btn').removeClass('active');
+        $(this).addClass('active');
     })
-
-    var item = $('.item');
 
     item.hover(function () {
             $(this).children().hide();
             $(this).css('background-image', $(this).children('.item-img').css('background-image'));
+            $(this).children('.tip_item').show();
         }, function () {
             $(this).children().show();
-            $(this).css('background-image', 'none')
+            $(this).css('background-image', 'none');
+            $(this).children('.tip_item').hide();
     })
 
     item.click(function () {
-
+        var id = $(this).attr('id');
+        if (order.includes(id)) {
+            order.splice(order.indexOf(id), 1)
+            $(this).children('.tip_item').html('+');
+        } else {
+            order.push($(this).attr('id'))
+            $(this).children('.tip_item').html('&times');
+        }
     })
 })
